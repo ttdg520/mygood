19a1f8cc2ce1a3a252fab5d5d25822d62075d89d
ttdg520 <93304517+ttdg520@users.noreply.github.com>
2024-12-24 15:11:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="tv.gaypan.cf";
+        url.hostname="api.xcffs.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
